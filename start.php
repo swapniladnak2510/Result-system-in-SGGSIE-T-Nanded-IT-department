697ea2b334243326e52fc59eb47022ea521d85cf
Swapnil Dhananjay Adnak <49309792+Loveguru-1999@users.noreply.github.com>
2019-08-28 21:31:17 +0530

    Add files via upload

---
+++ start.php
@@ -0,0 +1,181 @@
+<html lang="en">
+<head>
+<link rel="shortcut icon" type="image/png" href="icon.webp" 
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- font awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+  <title>Swapnil bhau</title>
+  <style>
+  header{
+    background:url(LOGO.PNG) no-repeat center center/cover;
+    min-height:1000px;
+
+  }
+  footer{
+    padding-bottom:40px;
+   
+  }
+  .image{
+    background-size:2px;
+  }
+  .now{
+    height:290px;
+    width:290px;
+  }
+  p{
+    letter-spacing:2px;
+    line-height:1.6;
+  }
+  i{
+    padding:5px;
+  }
+  @media screen and (max-width:700px){
+    header{
+      min-height:500px;
+    }
+  }
+  </style>
+</head>
+<body>
+    <header>
+        <nav class="nav-wraper transparent">
+            <div class="container">
+            <a href="#" class="brand-logo">SGGSIE&T</div></a>
+            <a href="#" class="sidenav-trigger" data-target="mobile">
+              <i class="material-icons">menu</i>
+            </a>
+        <ul class="right hide-on-med-and-down">
+          
+          <li><a href="testing.php">SGPA calculator</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contacts">Contacts</a></li>
+          <li><a href="#events">Events</a></li>
+        </ul>
+        </div>
+          </nav>
+        <ul class="sidenav" id="mobile">
+          
+            <li><a href="testing.php">SGPA calculator</a></li>
+            <li><a href="#about">About</a></li>
+          <li><a href="#contacts">Contacts</a></li>
+          <li><a href="#events">Events</a></li>
+        </ul>
+    </header>
+    <div class="container scrollspy" id="about">
+   
+    <h3>About</h3>
+    <div class="row">
+    <div class="col s12 l4">
+    <div class="card">
+    <div class="card-image">
+    <img src="sggs png.png" alt="sggs logo">
+    </div>
+    </div>
+    </div>
+    <div class="col s12 l7 offset-l1">
+    
+    <p>  Hello guys,I am currently pursuing b-tech in information technology in shri guru gobind singhji institute of engineering and technology(Nanded,Maharashtra),In our college when result get announced at that time we get only grades of respective subjects.We have to manually calculate the SGPA by doing some calculations.This is simple but time consuming task,hence here I developed site which calculate SGPA by just entering grades.</p>
+    </div>
+   </div>
+    </div>
+  <div class="parallax-container">
+    <div class="parallax">
+    <img src="star.jpg" >
+    </div>
+    </div>
+    <div class="container">
+    <h3>Hon.Director sir</h3>
+    <div class="row">
+    <div class="col s12 m9 offset-m2">
+   
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="director.jpg">
+        
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+        <p>Prof. Y. V. Joshi completed Bachelor of Electronics Engineering in 1986, Masters of Electronics Engineering in 1991 (both from SGGS Institute of Engineering and Technology, Vishnupuri, Nanded) and a Ph.D. in 1998 (from IIT, Delhi).  He is member of IEEE, Fellow of Institution of Engineers (India), Fellow of IETE (India) and a life member of ISTE.</p>
+        </div>
+        <div class="card-action "  >
+          <a href="https://sggs.ac.in/director/" class="cyan-text">More information</a>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+    </div>
+    </div>
+    <div class="parallax-container">
+    <div class="parallax">
+    <img src="star.jpg" >
+    </div>
+    </div>
+    <div class="container scrollspy" id="events">
+    <h3>Events in SGGSIE&T</h3>
+    <div class="row">
+    <div class="col s12 l12">
+    <div class="collection">
+        <a href="http://m.pragyaa.org/" class="collection-item">Pragya : Technical Event</a>
+        <a href="http://utsav17.pragyaa.org/" class="collection-item ">Utsav: : Cultural Event</a>
+        <a href="http://sggszenith.com/marathon/" class="collection-item">Zenith: Sport Event</a>
+        <a href="http://www.drishti-magazine.org/" class="collection-item">Drishti: Magazine Event</a>
+      </div>
+            
+    </div>
+   
+    </div>
+    </div>
+  <!-- navbar -->
+  
+  <!-- photo / grid -->
+
+  <!-- parallax -->
+
+  <!-- services / tabs -->
+
+  <!-- parallax -->
+
+  <!-- contact form -->
+<footer class="page-footer teal">
+ <div class="container scrollspy" id="contacts">
+ <h3>Connect me</h3>
+ <div class="row">
+ <div class="col offset-m4">
+ <a href="//www.linkedin.com/in/swapnil-adnak-a21184184" class="tooltipped btn-floating pink pulse" data-tooltip="Linkedin">
+ <i class="fab fa-linkedin" ></i>
+</a></div>
+ <div class="col offset-m1">
+ <a href="https://github.com/Loveguru-1999" class="tooltipped btn-floating pink pulse" data-tooltip="Github">
+ <i class="fab fa-github"></i>
+</a></div>
+<div class="col offset-m1">
+ <a href="https://www.hackerrank.com/swapnil25101999?hr_r=1" class="tooltipped btn-floating pink pulse" data-tooltip="Hackerrank">
+ <i class="">H</i>
+</a></div>
+ </div>
+ <div class="footer-copyright">
+ <div class="container center-align">&copy; 2019 Swapnil Dhananjay Adnak,Inc</div>
+ </div>
+</footer>
+  <!-- footer -->
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+  <script>
+    $(document).ready(function(){
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+    $('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy();
+    });
+  </script>
+</body>
+</html>
